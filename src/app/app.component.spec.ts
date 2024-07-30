2a92839faea9009867ae29c013e10ecb4b520eac
sisika0 <avatar008009mail.ru@gmail.com>
2024-07-31 02:10:18 +0300

    Add title change in project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'wayWayRegistration' title`, () => {
+  it(`should have the 'WayAlay' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wayWayRegistration');
+    expect(app.title).toEqual('WayAlay');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, wayWayRegistration',
+      'Hello, Home Page!',
     );
   });
 });
