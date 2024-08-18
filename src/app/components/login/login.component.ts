dcb141993abd2f69fb272213544f4dcae4ec6741
sisika0 <avatar008009mail.ru@gmail.com>
2024-08-18 13:37:22 +0300

    Reactive Froms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HlmInputModule } from '../ui-input-helm/src';
 import { HlmLabelDirective } from '../ui-label-helm/src/lib/hlm-label.directive';
 import { TelephoneInputComponent } from '../telephone-input/telephone-input.component';
@@ -13,26 +19,77 @@ import { DropdownComponent } from '../dropdown/dropdown.component';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     HlmInputModule,
     HlmLabelDirective,
     TelephoneInputComponent,
     DropdownComponent,
   ],
 })
-export class LoginComponent {
-  phone: string = '';
-  password: string = '';
-  confirmPassword: string = '';
-  passwordMatch: boolean = true;
-  isValidPhone: boolean = true;
-
-  phoneTouched: boolean = false;
-  passwordTouched: boolean = false;
-  confirmPasswordTouched: boolean = false;
-  formSubmitted: boolean = false;
-
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  formSubmitted = false;
   passwordVisible: boolean = false;
   confirmPasswordVisible: boolean = false;
+  currentIndex = 0;
+
+  photos = [
+    { id: 1, caption: 'Подпись 1' },
+    { id: 2, caption: 'Подпись 2' },
+    { id: 3, caption: 'Подпись 3' },
+    { id: 4, caption: 'Подпись 4' },
+    { id: 5, caption: 'Подпись 5' },
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group(
+      {
+        phone: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: this.passwordMatchValidator },
+    );
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null
+      : { mismatch: true };
+  }
+
+  onPhoneChange(event: { number: string; isValid: boolean }) {
+    console.log('Получено событие phoneChange:', event);
+
+    if (event.isValid) {
+      this.form.patchValue({ phone: event.number });
+      this.form.controls['phone'].setErrors(null);
+      console.log('Номер телефона установлен:', event.number);
+    } else {
+      this.form.controls['phone'].setErrors({ invalidPhone: true });
+      console.log('Номер телефона некорректен:', event.number);
+    }
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+
+    // Логируем значение перед отправкой формы
+    console.log('Значение формы:', this.form.value);
+
+    if (!this.form.controls['phone'].valid) {
+      console.error('Некорректный номер телефона.');
+      return;
+    }
+
+    if (this.form.valid) {
+      console.log('Form submitted:', this.form.value);
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 
   togglePasswordVisibility(field: string) {
     if (field === 'password') {
@@ -42,6 +99,7 @@ export class LoginComponent {
     }
   }
 
+<<<<<<< Updated upstream
   showModal: boolean = false;
   onSubmit() {
     const phoneInputElement = document.getElementById('phoneInput') as HTMLInputElement;
@@ -130,6 +188,8 @@ export class LoginComponent {
     { id: 5, caption: 'СТОМАТОЛОГ' },
   ];
 
+=======
+>>>>>>> Stashed changes
   onPrevClick() {
     if (this.currentIndex > 0) {
       this.currentIndex--;
--- src/app/components/telephone-input/telephone-input.component.ts
+++ src/app/components/telephone-input/telephone-input.component.ts
@@ -1,27 +1,30 @@
 import {
   Component,
+  AfterViewInit,
   ElementRef,
   EventEmitter,
   Output,
   ViewChild,
   ViewEncapsulation,
-  AfterViewInit,
 } from '@angular/core';
 import intlTelInput from 'intl-tel-input';
 
 @Component({
-  selector: 'app-telephone-input',
   standalone: true,
+  selector: 'app-telephone-input',
   templateUrl: './telephone-input.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./telephone-input.component.scss'],
 })
 export class TelephoneInputComponent implements AfterViewInit {
-  @Output() phoneChange = new EventEmitter<string>();
+  @Output() phoneChange = new EventEmitter<{
+    number: string;
+    isValid: boolean;
+  }>();
 
   @ViewChild('phoneInput', { static: true })
   phoneInput!: ElementRef<HTMLInputElement>;
-  private itiInstance: any;
+  itiInstance: any;
 
   ngAfterViewInit(): void {
     this.initializeIntlTelInput();
@@ -32,15 +35,28 @@ export class TelephoneInputComponent implements AfterViewInit {
       utilsScript:
         'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.15/js/utils.min.js',
       initialCountry: 'auto',
-      geoIpLookup: (callback: any) => {
+      separateDialCode: true,
+      geoIpLookup: (callback) => {
         fetch('https://ipapi.co/json/')
           .then((response) => response.json())
           .then((data) => callback(data.country_code))
           .catch(() => callback('us'));
       },
-      onlyCountries: ['by', 'ru', 'ua', 'pl', 'lv', 'lt', 'es'],
-      placeholderNumberType: 'MOBILE',
-      separateDialCode: true,
+    });
+
+    this.phoneInput.nativeElement.addEventListener('blur', () => {
+      const isValid = this.itiInstance.isValidNumber();
+      const number = this.itiInstance.getNumber();
+
+      // Логирование данных
+      console.log('Номер телефона из intl-tel-input:', number);
+      console.log('Валидность номера:', isValid);
+
+      if (isValid && number) {
+        this.phoneChange.emit({ number, isValid });
+      } else {
+        this.phoneChange.emit({ number: '', isValid: false });
+      }
     });
   }
 }
